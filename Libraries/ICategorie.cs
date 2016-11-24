92f399e8fbc8313be4008a5cc6a1f945db111df9
Félicien Schiltz <felicien.schiltz@gmail.com>
2016-11-24 17:13:05 +0100

    Implementation of categorie services + datacontracts

---
+++ Libraries/ICategorie.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.ServiceModel;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Libraries
+{
+    public interface ICategorie
+    {
+        [OperationContract]
+        Categorie getCategorie(int Id);
+
+        [OperationContract]
+        List<Categorie> searchCategories(string pattern);
+
+        [OperationContract]
+        Categorie seeDetails(int Id);
+
+        [OperationContract]
+        Boolean modify(Categorie categorie);
+
+        [OperationContract]
+        Boolean delete(int id);
+    }
+}
